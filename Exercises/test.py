ec35644f56bd5f2e65cc644f9cfe638cd1b66601
Adam Hedlund <adamhedlund423@gmail.com>
2025-09-16 14:49:41 +0200

    Done

--- Exercises/test.py
+++ Exercises/test.py
@@ -1,4 +1,12 @@
-import random
+epsilon = 1e-6
+summa = 0.0
+term = 1.0
+n = 0
 
-nummer = random.randint(1,100)
-print(nummer)
+while term > epsilon:
+    summa += term
+    term /= 2
+    n += 1
+
+print(f"Summan blir ungefär {summa}")
+print(f"Antal termer: {n}")
