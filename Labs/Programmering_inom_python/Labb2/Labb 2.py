97e0a5ce12c44240fcec866fe3ee6c46bcebc75f
Adam Hedlund <adamhedlund423@gmail.com>
2025-09-25 10:08:43 +0200

    #Updated functional code to determine nn with K=1

--- Labs/Programmering_inom_python/Labb2/Labb 2.py
+++ Labs/Programmering_inom_python/Labb2/Labb 2.py
@@ -1,6 +1,103 @@
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv("python-programming-Adam-Hedlund\Labs\Programmering_inom_python\Labb2\datapoints.txt")
+width = []
+height = []
+label = []
 
-print(df.head())
+with open("python-programming-Adam-Hedlund\Labs\Programmering_inom_python\Labb2\datapoints.txt") as fil:
+    next(fil)
+    for line in fil:
+        parts = line.strip().split(",")
+        if len(parts) == 3:
+            width.append(float(parts[0]))
+            height.append(float(parts[1]))
+            label.append(float(parts[2]))
 
+data = np.array(list(zip(width, height, label)))
+
+test_x = []
+test_y= []
+
+with open(r"python-programming-Adam-Hedlund\Labs\Programmering_inom_python\Labb2\testpoints.txt")as test:
+    next(test)
+    for rad in test:
+        rad = rad.strip()
+        if "(" in rad and ")" in rad:
+            testcoordinates_text = rad.split("(")[1].split(")")[0]
+            x_val , y_val = testcoordinates_text.split(",")
+            test_x.append(float(x_val))
+            test_y.append(float(y_val))
+        
+
+
+training_points = np.array(list(zip(width, height)))
+pichu = data[data[:,2] == 0]
+pikachu = data[data[:,2] == 1]
+distances = []   
+pred_labels = []
+nn_idx =[]
+
+testpoints = np.array(list(zip(test_x,test_y)))
+labels_np = np.asanyarray(label)
+name_map ={0: "Pichu", 1:"Pikachu"}
+
+for q in testpoints:
+    d = np.linalg.norm(training_points - q, axis =1)
+    j = int(np.argmin(d))
+    y = int(labels_np[j])
+
+    pred_labels.append(int(y))
+    nn_idx.append(int(j))
+    #print(f"{q} is classified as {name_map.get(y)}")
+
+while True:
+    try:
+        user_input_x = float(input("Skriv in ett positivt x-värde: "))
+        user_input_y = float(input("Skriv in positivt y-värde: "))
+        
+        if user_input_x > 0 and user_input_y > 0:
+            print(f"Du angav x = {user_input_x}  och = y {user_input_y}!")
+            break
+        else:
+            print("Båda talen måste vara större än 0, försök igen!")
+    except ValueError:
+        print("Du måste ange ett tal (heltal/decimaltal), försök igen!")
+
+user_input_point = np.array([user_input_x, user_input_y])
+
+
+d = np.linalg.norm(training_points - user_input_point, axis =1)
+j = int(np.argmin(d))
+y = int(labels_np[j])
+pred_labels.append(int(y))
+nn_idx.append(int(j))
+print(f"User input:{user_input_point} is classified as {name_map.get(y)}")
+
+k = 10
+
+for q in user_input_point:
+    d = np.linalg.norm(training_points - q, axis=1)
+    k_indices = np.argsort(d)[:k]
+    k_labels = labels_np[k_indices]
+    counts = np.bincount(k_labels.astype(int))
+    y = np.argmax(counts)
+    pred_labels.append(int(y))
+    nn_idx.append(k_indices)
+
+    print(f"User input: {user_input_point} is classified as {name_map.get(y)}")
+
+    
+    
+    
+
+plt.scatter(pichu[:,0], pichu[:,1], label="Pichu", color="blue")
+plt.scatter(pikachu[:,0], pikachu[:,1], label="Pickachu", color="Yellow")
+plt.scatter(test_x, test_y, label="Testpunkter", color="red")
+plt.scatter(user_input_point[0], user_input_point[1], label=f"{user_input_point}={name_map.get(y)}", marker="*", color="green")
+plt.xlabel("Width (cm)")
+plt.ylabel("Height (cm)")
+plt.title("Pichu vs Pikachu")
+plt.legend()
+plt.grid(True)
+plt.show()
